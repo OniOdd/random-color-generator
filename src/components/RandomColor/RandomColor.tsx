dcaee289733bb8322ed8dd551f166aa65e0daf6a
Mykyta Kavetskyi <103828218+OniOdd@users.noreply.github.com>
2024-03-15 00:31:22 +0200

    project ready

--- src/components/RandomColor/RandomColor.tsx
+++ src/components/RandomColor/RandomColor.tsx
@@ -1,14 +1,55 @@
 import style from './RandomColor.module.css';
+import { useState, useEffect } from 'react';
 
 function RandomColor() {
+  const [colorType, setColorType] = useState('hex');
+  const [color, setColor] = useState('#000000');
+
+  useEffect(() => {
+    colorType === 'hex' ?
+      handleCreateRandomHexColor() :
+      handleCreateRandomRgbColor();
+  }, [colorType]);
+
+  function generateRandomColor(value: number) {
+    return Math.floor(Math.random() * value);
+  }
+
+  function handleCreateRandomHexColor() {
+    const hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    let hexColor = '#';
+
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[generateRandomColor(hex.length)];
+    }
+
+    setColor(hexColor);
+  }
+
+  function handleCreateRandomRgbColor() {
+    const red = generateRandomColor(256);
+    const green = generateRandomColor(256);
+    const blue = generateRandomColor(256);
+
+    setColor(`rgb(${red}, ${green}, ${blue})`);
+  }
+
   return (
-    <div className={style.container}>
-      <button className={style.button} type="button">Create HEX Color</button>
-      <button className={style.button} type="button">Create RGB Color</button>
-      <button className={style.button} type="button">Generate Random Color</button>
-      <section>
-        <h1></h1>
-        <h2></h2>
+    <div className={style.container} style={{backgroundColor: color}}>
+      <button className={style.button} type="button"
+              onClick={() => setColorType('hex')}
+      >Create HEX Color</button>
+      <button className={style.button} type="button"
+              onClick={() => setColorType('rgb')}
+      >Create RGB Color</button>
+      <button className={style.button} type="button"
+              onClick={colorType === 'hex' ?
+                        handleCreateRandomHexColor :
+                        handleCreateRandomRgbColor}
+      >Generate Random Color</button>
+      <section className={style.display}>
+        <h1>{colorType === 'hex' ? 'HEX Color' : 'RGB Color'}</h1>
+        <h2>{color}</h2>
       </section>
     </div>
   );
